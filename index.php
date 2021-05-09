1d336294d13483bc55ab1035cc35133c0ad5173f
spsapa <ytyonti@gmail.com>
2021-05-10 02:16:13 +0900

    asd
    asd

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang='ko'>
+  <head>
+  	<meta charset="utf-8">
+	  <link rel="preconnect" href="https://fonts.gstatic.com">
+	  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet">
+	  <meta name="viewport" content="width=750px">
+	  <title>커미션 사이트(?)</title>
+    <link href='global.css' ref='stylesheet' type='text/css'>
+	  <link href="header.css" rel="stylesheet" type="text/css">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  </head>
+  <body>
+  	<?php require('header.php'); ?>
+  <script src="script.js"></script>
+  <div class='conbutton'>
+  <h1>전신</h1>
+  <h1>흉상</h1>
+  </div>
+  </body>
+</html>
